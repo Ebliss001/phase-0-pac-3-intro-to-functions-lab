94eecc4573d3d87dbc9861952fda1f1ca8e87392
sunday ebenezer <sundayebenezer8@gmail.com>
2024-03-08 15:06:23 -0800

    phase-0-pac-3-intro-to-functions-lab

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+   return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+function logShout (string) {
+console.log("Hello".toUpperCase());
+}
+
+function logwhisper(string) {
+  console.log("HELLO".toLowerCase());
+}
+console.log("HELLO");
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToHeadPhonedRoomate(string) {
+  console.log(sayHiToHeadPhonedRoomate("Hi"));
+  if (string.toLowerCase().equals("hello")) {
+    return 'I cant hear you!'
+} else if (string.toUpperCase().equals("HELLO")) {
+    return 'YES INDEED!'
+} 
+}
+  
